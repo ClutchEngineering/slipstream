e98ebaff64368ecfd63b9bdfc1a203addf91f878
Jeff Verkoeyen <jverkoey@gmail.com>
2024-07-31 14:11:26 -0400

    Add README and logo.

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "slipstream",
+  targets: [
+    .executableTarget(name: "slipstream"),
+  ]
+)
---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+  <img alt="Tailwind CSS" src="https://raw.githubusercontent.com/jverkoey/slipstream/HEAD/.github/logo.svg" width="350" height="70" style="max-width: 100%;">
+</p>
+
+<p align="center">
+  Build static web pages with Swift and Tailwind CSS.
+</p>
