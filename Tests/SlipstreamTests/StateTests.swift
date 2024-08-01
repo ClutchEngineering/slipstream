33e9b96b1e042247903390ef2af63118c98a0f4b
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-01 15:48:52 -0400

    Add state tests and move Text tests into a sub-directory. (#10)

---
+++ Tests/SlipstreamTests/StateTests.swift
@@ -0,0 +1,17 @@
+import Testing
+
+import Slipstream
+
+private struct StateView: View {
+  let string: String
+  var body: some View {
+    Text(string)
+  }
+}
+
+struct StateTests {
+  @Test func rendersProvidedString() throws {
+    try #expect(renderHTML(StateView(string: "Hello, world!")) == "Hello, world!")
+    try #expect(renderHTML(StateView(string: "Slipstream")) == "Slipstream")
+  }
+}
