699c2c878040b81d814f16b3542e93d11124a464
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-02 13:13:20 -0400

    Tune HTML's documentation.
    Add an example and link to related APIs.

--- Sources/Slipstream/Views/W3C/Elements/HTML.swift
+++ Sources/Slipstream/Views/W3C/Elements/HTML.swift
@@ -2,7 +2,23 @@ import SwiftSoup
 
 /// The html element represents the root of a document.
 ///
+/// Every web page starts with an HTML view.
+///
+/// ```swift
+/// struct MySite: View {
+///   var body: some View {
+///     HTML {
+///       // Your site's content.
+///     }
+///   }
+/// }
+/// ```
+///
 /// View the full [W3C specification](https://www.w3.org/TR/2012/WD-html-markup-20121025/html.html#html).
+///
+/// ## See Also
+///
+/// - ``View/language(_:)``
 @available(iOS 17.0, macOS 14.0, *)
 public struct HTML<Content>: View where Content: View {
   public init(@ViewBuilder content: @escaping () -> Content) {
