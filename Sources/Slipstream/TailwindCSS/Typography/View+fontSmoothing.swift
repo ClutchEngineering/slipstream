19785d2b329f56a100ca4ee2fc541e22f837b806
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-03 14:42:36 -0400

    Add fontSmoothing and antialiased modifiers. (#62)
    Part of https://github.com/jverkoey/slipstream/issues/51.

---
+++ Sources/Slipstream/TailwindCSS/Typography/View+fontSmoothing.swift
@@ -0,0 +1,28 @@
+/// Constants that specify font smoothing.
+///
+/// - SeeAlso: Tailwind CSS' [`font-smoothing`](https://tailwindcss.com/docs/font-smoothing) documentation.
+@available(iOS 17.0, macOS 14.0, *)
+public enum FontSmoothing: String {
+  case antialiased
+  case subpixelAntialiased = "subpixel-antialiased"
+}
+
+extension View {
+  /// Set the font smoothing for a view.
+  ///
+  /// - Parameter fontSmoothing: The font smoothing to apply to the modified view.
+  ///
+  /// - SeeAlso: Tailwind CSS' [`font-smoothing`](https://tailwindcss.com/docs/font-smoothing) documentation.
+  @available(iOS 17.0, macOS 14.0, *)
+  public func fontSmoothing(_ fontSmoothing: FontSmoothing) -> some View {
+    return self.modifier(ClassModifier(add: fontSmoothing.rawValue))
+  }
+
+  /// Set the font smoothing for a view to antialiased.
+  ///
+  /// - SeeAlso: Tailwind CSS' [`font-smoothing`](https://tailwindcss.com/docs/font-smoothing) documentation.
+  @available(iOS 17.0, macOS 14.0, *)
+  public func antialiased() -> some View {
+    return fontSmoothing(.antialiased)
+  }
+}
