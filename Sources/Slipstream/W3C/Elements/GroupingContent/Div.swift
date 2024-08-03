e43990a4437a617a2239052790248c9c4f6d8e47
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-03 19:38:00 -0400

    Typo.

--- Sources/Slipstream/W3C/Elements/GroupingContent/Div.swift
+++ Sources/Slipstream/W3C/Elements/GroupingContent/Div.swift
@@ -1,7 +1,7 @@
 /// A view that has no special meaning at all.
 ///
 /// A Div represents its children. Modifiers applied to a Div will
-/// propgate to its children.
+/// propagate to its children.
 ///
 /// ```swift
 /// struct MySiteContent: View {
