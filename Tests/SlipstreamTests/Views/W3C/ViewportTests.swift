8cfd5cb34bb6a668ab53fd54cbc4bd1d9630b273
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-02 21:40:47 -0400

    Add meta element viewport implementation. (#39)
    Part of https://github.com/jverkoey/slipstream/issues/25.

---
+++ Tests/SlipstreamTests/Views/W3C/ViewportTests.swift
@@ -0,0 +1,28 @@
+import Testing
+
+import Slipstream
+
+struct ViewportTests {
+  @Test func defaults() throws {
+    try #expect(renderHTML(Viewport()) == #""#)
+  }
+
+  @Test func mobileStandard() throws {
+    try #expect(renderHTML(Viewport.mobileFriendly) == #"<meta name="viewport" content="width=device-width, initial-scale=1" />"#)
+  }
+
+  @Test func fullyConfigured() throws {
+    try #expect(
+      renderHTML(
+        Viewport(
+          width: .pixels(500),
+          height: .deviceHeight,
+          initialScale: 2.0,
+          minimumScale: 0.5,
+          maximumScale: 5.0,
+          userScalable: false
+        )
+      ) == #"<meta name="viewport" content="width=500, height=device-height, initial-scale=2, minimum-scale=0.5, maximum-scale=5, user-scalable=no" />"#
+    )
+  }
+}
