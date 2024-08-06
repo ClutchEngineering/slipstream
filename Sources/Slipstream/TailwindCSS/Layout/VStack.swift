78d878d8032fe8469afa3ccaaaaa31bf466604d3
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-06 15:28:21 -0400

    Add alignment to HStack/VStack and default it to center.

--- Sources/Slipstream/TailwindCSS/Layout/VStack.swift
+++ Sources/Slipstream/TailwindCSS/Layout/VStack.swift
@@ -16,16 +16,27 @@
 ///   }
 /// }
 /// ```
+/// 
+/// ## See Also
+///
+/// - ``AlignItems``
 @available(iOS 17.0, macOS 14.0, *)
 public struct VStack<Content: View>: View {
   /// Creates an HStack view.
   ///
   /// - Parameters:
+  ///   - alignment: Determines how items within the stack are positioned along the x axis.
   ///   - spacing: If provided, the amount of spacing to add between child views. The value is
   ///   expressed in points, and mapped to the closest Tailwind CSS spacing class.
   ///   - reversed: If true, the contents will be arranged vertically from bottom to top.
   ///   - content: The content to display with this view.
-  public init(spacing: Double? = nil, reversed: Bool = false, @ViewBuilder content: @escaping () -> Content) {
+  public init(
+    alignment: AlignItems = .center,
+    spacing: Double? = nil,
+    reversed: Bool = false,
+    @ViewBuilder content: @escaping () -> Content
+  ) {
+    self.alignment = alignment
     self.spacing = spacing
     self.reversed = reversed
     self.content = content
@@ -36,6 +47,7 @@ public struct VStack<Content: View>: View {
     let container = Div { content() }
       .display(.flex)
       .flexDirection(.y, reversed: reversed)
+      .alignItems(alignment)
     if let spacing {
       container
         .flexGap(.y, width: spacing)
@@ -44,6 +56,7 @@ public struct VStack<Content: View>: View {
     }
   }
 
+  private let alignment: AlignItems
   private let spacing: Double?
   private let reversed: Bool
   private let content: () -> Content
--- Tests/SlipstreamTests/TailwindCSS/Layout/HStackTests.swift
+++ Tests/SlipstreamTests/TailwindCSS/Layout/HStackTests.swift
@@ -4,15 +4,15 @@ import Slipstream
 
 struct HStackTests {
   @Test func emptyBlock() throws {
-    try #expect(renderHTML(HStack {}) == #"<div class="flex flex-row"></div>"#)
-    try #expect(renderHTML(HStack(reversed: true) {}) == #"<div class="flex flex-row-reverse"></div>"#)
+    try #expect(renderHTML(HStack {}) == #"<div class="flex flex-row items-center"></div>"#)
+    try #expect(renderHTML(HStack(reversed: true) {}) == #"<div class="flex flex-row-reverse items-center"></div>"#)
   }
 
   @Test func withText() throws {
     try #expect(renderHTML(HStack {
       Text("Hello, world!")
     }) == """
-<div class="flex flex-row">
+<div class="flex flex-row items-center">
  Hello, world!
 </div>
 """)
