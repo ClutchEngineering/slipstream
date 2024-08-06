bb627ae7ac8116b855ffd45fe6ae2a8404f6017b
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-05 22:09:20 -0400

    Add Blockquote element.
    Part of https://github.com/jverkoey/slipstream/issues/25.

--- Sources/Slipstream/W3C/Elements/GroupingContent/Blockquote.swift
+++ Sources/Slipstream/W3C/Elements/GroupingContent/Blockquote.swift
@@ -1,30 +1,32 @@
-/// A view that represents a paragraph.
+/// A view that represents a section that is quoted from another source.
 ///
 /// ```swift
 /// struct MySiteContent: View {
 ///   var body: some View {
 ///     Body {
-///       Paragraph("Hello, world!")
+///       Blockquote {
+///         Text("Hello, world!")
+///       }
 ///     }
 ///   }
 /// }
 /// ```
 ///
-/// - SeeAlso: W3C [`p`](https://html.spec.whatwg.org/multipage/grouping-content.html#the-p-element) specification.
+/// - SeeAlso: W3C [blockquote](https://html.spec.whatwg.org/multipage/grouping-content.html#the-blockquote-element) specification.
 @available(iOS 17.0, macOS 14.0, *)
-public struct Paragraph<Content>: W3CElement where Content: View {
+public struct Blockquote<Content>: W3CElement where Content: View {
   @_documentation(visibility: private)
-  public let tagName: String = "p"
+  public let tagName: String = "blockquote"
 
   @_documentation(visibility: private)
   @ViewBuilder public let content: () -> Content
 
-  /// Creates a paragraph.
+  /// Creates a blockquote.
   public init(@ViewBuilder content: @escaping () -> Content) {
     self.content = content
   }
 
-  /// Creates a paragraph with some static text.
+  /// Creates a blockquote with some static text.
   public init(_ text: String) where Content == Text {
     self.content = {
       Text(text)
