d74d98a2a0ea0f46a45d14bd27ea50d152dd02ca
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-06 21:28:05 -0400

    Add Preformatted element.
    Part of https://github.com/jverkoey/slipstream/issues/25.

--- Sources/Slipstream/Documentation.docc/W3C/W3CViews.md
+++ Sources/Slipstream/Documentation.docc/W3C/W3CViews.md
@@ -37,6 +37,7 @@ The complete W3C HTML elements standard can be found [here](https://html.spec.wh
 
 - ``Paragraph``
 - ``Divider``
+- ``Preformatted``
 - ``Blockquote``
 - ``List``
 - ``ListItem``
---
+++ Sources/Slipstream/W3C/Elements/GroupingContent/Preformatted.swift
@@ -0,0 +1,35 @@
+/// A view that represents a block of preformatted text.
+///
+/// ```swift
+/// struct MySiteContent: View {
+///   var body: some View {
+///     Body {
+///       Preformatted("""
+/// print("Hello, world!")
+/// """)
+///     }
+///   }
+/// }
+/// ```
+///
+/// - SeeAlso: W3C [`pre`](https://html.spec.whatwg.org/multipage/grouping-content.html#the-pre-element) specification.
+@available(iOS 17.0, macOS 14.0, *)
+public struct Preformatted<Content>: W3CElement where Content: View {
+  @_documentation(visibility: private)
+  public let tagName: String = "pre"
+
+  @_documentation(visibility: private)
+  @ViewBuilder public let content: () -> Content
+
+  /// Creates a preformatted view.
+  public init(@ViewBuilder content: @escaping () -> Content) {
+    self.content = content
+  }
+
+  /// Creates a preformatted view with some static text.
+  public init(_ text: String) where Content == Text {
+    self.content = {
+      Text(text)
+    }
+  }
+}
