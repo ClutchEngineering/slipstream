1e9a182ed920cb936013522431ed1bd322b94199
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-07 20:41:11 -0400

    Add intro section for the tutorial.

---
+++ Sources/Slipstream/Documentation.docc/Tutorials/IntroducingSlipstream/IntroducingSlipstream-YourFirstSite-1-4.sh
@@ -0,0 +1,2 @@
+cd ~/path/to/your/site
+npx tailwindcss init
---
+++ Sources/Slipstream/Documentation.docc/Tutorials/IntroducingSlipstream/IntroducingSlipstream-YourFirstSite-2-2.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "MySite",
+  dependencies: [
+    .package(url: "https://github.com/jverkoey/slipstream.git", branch: "main"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(name: "MySite", dependencies: [
+      .product(name: "Slipstream", package: "slipstream"),
+    ]),
+  ]
+)
