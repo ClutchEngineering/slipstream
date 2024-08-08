b740a3ea02ecf87f7f4155a7af07b933446a89fa
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-08 09:29:07 -0400

    Add TextImageLink section to the tutorial.

---
+++ Sources/Slipstream/Documentation.docc/Tutorials/IntroducingSlipstream/YourFirstSite/TextImageLink/IntroducingSlipstream-YourFirstSite-TextImageLink-2-2.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+import Slipstream
+
+struct Home: View {
+  var body: some View {
+    HTML {
+      Head {
+        Stylesheet(URL(string: "main.css"))
+      }
+      Body {
+        Container {
+          Text("Meet Coco the pig. Coco is a swimming pig from the Bahamas!")
+          Link(
+            "Learn more",
+            destination: URL(string: "https://www.bahamas.com/experiences/official-home-swimming-pigs")
+          )
+          .textColor(Color(.blue, darkness: 500))
+          .underline(condition: .hover)
+          Image(URL(string: "coco.jpeg"))
+        }
+      }
+    }
+  }
+}
+
+let sitemap: Sitemap = [
+  "index.html": Home()
+]
+
+guard let projectURL = URL(filePath: #filePath)?
+  .deletingLastPathComponent()
+  .deletingLastPathComponent() else {
+  print("Unable to create URL for \(#filePath)")
+  exit(1)
+}
+
+let outputURL = projectURL.appending(path: "site")
+
+try renderSitemap(sitemap, to: outputURL)
