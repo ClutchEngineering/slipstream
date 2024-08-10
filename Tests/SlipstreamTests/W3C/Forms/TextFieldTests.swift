1bc7a39ad527197523a3e5d5aca72f286b431a6b
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-10 09:56:33 -0400

    [feature] Add autofocus support to TextField.

--- Tests/SlipstreamTests/W3C/Forms/TextFieldTests.swift
+++ Tests/SlipstreamTests/W3C/Forms/TextFieldTests.swift
@@ -6,4 +6,8 @@ struct TextFieldTests {
   @Test func placeholder() throws {
     try #expect(renderHTML(TextField("Placeholder")) == #"<input type="text" placeholder="Placeholder" />"#)
   }
+
+  @Test func autoFocus() throws {
+    try #expect(renderHTML(TextField("Placeholder", autoFocus: true)) == #"<input type="text" placeholder="Placeholder" autofocus />"#)
+  }
 }
