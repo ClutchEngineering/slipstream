7734f826fee14884ded2cc5ce25057f7e4edd3d6
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-10 00:25:41 -0400

    Add support for opacity to Color. (#143)

--- Tests/SlipstreamTests/TailwindCSS/Backgrounds/BackgroundColorTests.swift
+++ Tests/SlipstreamTests/TailwindCSS/Backgrounds/BackgroundColorTests.swift
@@ -13,4 +13,10 @@ struct BackgroundColorTests {
   @Test func colorWithDarkness() throws {
     try #expect(renderHTML(Div {}.background(.cyan, darkness: 500)) == #"<div class="bg-cyan-500"></div>"#)
   }
+
+  @Test func colorWithOpacity() throws {
+    try #expect(renderHTML(Div {}.background(.init(.cyan, darkness: 500).opacity(0))) == #"<div class="bg-cyan-500/0"></div>"#)
+    try #expect(renderHTML(Div {}.background(.init(.cyan, darkness: 500).opacity(0.5))) == #"<div class="bg-cyan-500/50"></div>"#)
+    try #expect(renderHTML(Div {}.background(.init(.cyan, darkness: 500).opacity(20))) == #"<div class="bg-cyan-500"></div>"#)
+  }
 }
