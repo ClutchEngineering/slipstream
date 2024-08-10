4fbe98a8e4ba4eaa326e1f48d0e31bcc7eb192ef
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-10 09:58:21 -0400

    [feature] Add autofocus support to TextField. (#156)

--- Sources/Slipstream/W3C/Elements/Forms/TextField.swift
+++ Sources/Slipstream/W3C/Elements/Forms/TextField.swift
@@ -6,8 +6,15 @@ import SwiftSoup
 @available(iOS 17.0, macOS 14.0, *)
 public struct TextField: View {
   /// Creates a text field with a text label.
-  public init(_ text: String) {
+  ///
+  /// - Parameters:
+  ///   - text: The placeholder text to display in the text field when it is empty.
+  ///   - autoFocus: If true, indicates that the view should be focused as soon as
+  ///   the page is loaded, allowing the user to start typing without having to
+  ///   manually focus the main view.
+  public init(_ text: String, autoFocus: Bool = false) {
     self.text = text
+    self.autoFocus = autoFocus
   }
 
   @_documentation(visibility: private)
@@ -15,7 +22,11 @@ public struct TextField: View {
     let element = try container.appendElement("input")
     try element.attr("type", "text")
     try element.attr("placeholder", text)
+    if autoFocus {
+      try element.attr("autofocus", "")
+    }
   }
 
   private let text: String
+  private let autoFocus: Bool
 }
