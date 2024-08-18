7a65597caf32aae2ed47239468335f0a6ebd0eed
Jeff Verkoeyen <jverkoey@gmail.com>
2024-08-17 20:03:17 -0400

    Add the ability to recurse into detached Markup nodes.

--- Tests/SlipstreamTests/Markdown/MarkdownTests.swift
+++ Tests/SlipstreamTests/Markdown/MarkdownTests.swift
@@ -3,12 +3,52 @@ import Testing
 import Markdown
 import Slipstream
 
+private enum TableContext {
+  case header
+  case body
+}
+
+private struct TableContextEnvironmentKey: EnvironmentKey {
+  static let defaultValue: TableContext = .body
+}
+
+extension EnvironmentValues {
+  fileprivate var tableContext: TableContext {
+    get { self[TableContextEnvironmentKey.self] }
+    set { self[TableContextEnvironmentKey.self] = newValue }
+  }
+}
+
+private struct ContextAwareTableCell<Content: View>: View {
+  @Environment(\.tableContext) var tableContext
+
+  @ViewBuilder let content: () -> Content
+
+  var body: some View {
+    switch tableContext {
+    case .header:
+      Slipstream.TableHeaderCell {
+        content()
+      }
+    case .body:
+      Slipstream.TableCell {
+        content()
+      }
+    }
+  }
+}
+
 private struct MarkdownArticle: View {
   var body: some View {
     MarkdownText("""
 ## Hello world
 
 This is some markdown content.
+
+| Column 1 | Column 2 |
+|:---------|:---------|
+| Row 1a | Row 1b |
+| Row 2a | Row 2b |
 """
     ) { node, context in
       switch node {
@@ -20,6 +60,23 @@ This is some markdown content.
         }
       case is Markdown.Paragraph:
         Slipstream.Paragraph {
+          context.recurse()
+        }
+      case let table as Markdown.Table:
+        Slipstream.Table {
+          Slipstream.TableHeader {
+            context.recurseDetached(into: table.head)
+          }
+          Slipstream.TableBody {
+            context.recurseDetached(into: table.body)
+          }
+        }
+      case is Markdown.Table.Row:
+        Slipstream.TableRow {
+          context.recurse()
+        }
+      case is Markdown.Table.Cell:
+        ContextAwareTableCell {
           context.recurse()
         }
       default:
@@ -34,6 +91,22 @@ struct MarkdownTests {
     try #expect(renderHTML(MarkdownArticle()) == """
 <h2>Hello world</h2>
 <p>This is some markdown content.</p>
+<table>
+ <thead>
+  <td>Column 1</td>
+  <td>Column 2</td>
+ </thead>
+ <tbody>
+  <tr>
+   <td>Row 1a</td>
+   <td>Row 1b</td>
+  </tr>
+  <tr>
+   <td>Row 2a</td>
+   <td>Row 2b</td>
+  </tr>
+ </tbody>
+</table>
 """)
   }
 }
