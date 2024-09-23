7a235e9cb846dcf1dbda3220a5734aae8c52ee4d
Jeff Verkoeyen <jverkoey@gmail.com>
2024-09-22 18:29:06 -0700

    Add support for colspan and rowspan on TableCell.

--- Tests/SlipstreamTests/W3C/TabularData/TableCellTests.swift
+++ Tests/SlipstreamTests/W3C/TabularData/TableCellTests.swift
@@ -7,6 +7,12 @@ struct TableCellTests {
     try #expect(renderHTML(TableCell {}) == "<td></td>")
   }
 
+  @Test func spans() throws {
+    try #expect(renderHTML(TableCell(rowSpan: 2) {}) == #"<td rowspan="2"></td>"#)
+    try #expect(renderHTML(TableCell(colSpan: 3) {}) == #"<td colspan="3"></td>"#)
+    try #expect(renderHTML(TableCell(rowSpan: 2, colSpan: 4) {}) == #"<td rowspan="2" colspan="4"></td>"#)
+  }
+
   @Test func withText() throws {
     try #expect(renderHTML(TableCell {
       DOMString("Hello, world!")
