9ce48ca56a5d87187e5ee68be72091819b35899c
Jeff Verkoeyen <jeff@clutch.engineering>
2024-09-22 18:32:55 -0700

    Add support for colspan and rowspan on TableCell. (#203)

--- Sources/Slipstream/W3C/Elements/TabularData/TableCell.swift
+++ Sources/Slipstream/W3C/Elements/TabularData/TableCell.swift
@@ -1,16 +1,30 @@
+import SwiftSoup
+
 /// A view that represents a data cell in a table.
 ///
 /// - SeeAlso: W3C [td](https://html.spec.whatwg.org/multipage/tables.html#the-td-element) specification.
 @available(iOS 17.0, macOS 14.0, *)
-public struct TableCell<Content>: W3CElement where Content: View {
-  @_documentation(visibility: private)
-  public let tagName: String = "td"
-
-  @_documentation(visibility: private)
-  @ViewBuilder public let content: () -> Content
-
+public struct TableCell<Content>: View where Content: View {
   /// Creates a table cell.
-  public init(@ViewBuilder content: @escaping () -> Content) {
+  public init(rowSpan: Int? = nil, colSpan: Int? = nil, @ViewBuilder content: @escaping () -> Content) {
     self.content = content
+    self.rowSpan = rowSpan
+    self.colSpan = colSpan
   }
+
+  @_documentation(visibility: private)
+  public func render(_ container: Element, environment: EnvironmentValues) throws {
+    let element = try container.appendElement("td")
+    if let rowSpan {
+      try element.attr("rowspan", "\(rowSpan)")
+    }
+    if let colSpan {
+      try element.attr("colspan", "\(colSpan)")
+    }
+    try self.content().render(element, environment: environment)
+  }
+
+  @ViewBuilder private let content: () -> Content
+  private let rowSpan: Int?
+  private let colSpan: Int?
 }
