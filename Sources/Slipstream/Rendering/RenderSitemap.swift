48d428abedce688d99f015d931fb4ff1153fcdce
csjones <csjones@users.noreply.github.com>
2025-11-11 19:02:33 -0800

    Restore original renderSitemap

--- Sources/Slipstream/Rendering/RenderSitemap.swift
+++ Sources/Slipstream/Rendering/RenderSitemap.swift
@@ -8,21 +8,15 @@ import SwiftSoup
 /// - Parameter encoding: The encoding to use when saving files to disk.
 /// - Throws: A SwiftSoup `Exception.Error` may be thrown if a failure occurs while rendering the view.
 public func renderSitemap(_ sitemap: Sitemap, to folder: URL, encoding: String.Encoding = .utf8) throws {
-    let environment = EnvironmentValues()
-    
-    // Render HTML pages
-    for (path, view) in sitemap.sorted(by: { $0.key < $1.key }) {
-        let document = Document("/")
-        try view.render(document, environment: environment)
-        let output = try "<!DOCTYPE html>\n" + document.html()
-        
-        let fileURL = folder.appending(path: path)
-        let folderURL = fileURL.deletingLastPathComponent()
-        if !FileManager.default.fileExists(atPath: folderURL.path(percentEncoded: false)) {
-            try FileManager.default.createDirectory(at: folderURL, withIntermediateDirectories: true)
-        }
-        try output.write(to: fileURL, atomically: true, encoding: encoding)
+  for (path, view) in sitemap.sorted(by: { $0.key < $1.key }) {
+    let output = try "<!DOCTYPE html>\n" + renderHTML(view)
+    let fileURL = folder.appending(path: path)
+    let folderURL = fileURL.deletingLastPathComponent()
+    if !FileManager.default.fileExists(atPath: folderURL.path(percentEncoded: false)) {
+      try FileManager.default.createDirectory(at: folderURL, withIntermediateDirectories: true)
     }
+    try output.write(to: fileURL, atomically: true, encoding: encoding)
+  }
 }
 
 /// Renders the given sitemap to a folder with CSS component collection.
